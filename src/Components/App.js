535081aaa07b3139a3d57c77f319be8d92170df8
Ge0frey <jeffkimzyjoe@gmail.com>
2024-03-08 21:23:00 +0300

    Fetches updated data from the api and not the localstorage

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import api from "../api/contacts"
 import AddContact from './AddContact';
 import Header from './Header';
 import ContactList from './ContactList';
@@ -12,6 +13,12 @@ console.log(Uuid);
 function App () {
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+
+  //retrieve contacts
+  const retrieveContacts = async () => {
+    const response = await api.get("/contacts");
+    return response.data;
+  }
 
   const addContactHandler = (contact) => {
     console.log(contact)
@@ -27,9 +34,16 @@ function App () {
   }
 
   useEffect(() => {
-    const retrievedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    console.log('Retrieved Contacts:', retrievedContacts);
-    if (retrievedContacts) setContacts(retrievedContacts);
+    // const retrievedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    // console.log('Retrieved Contacts:', retrievedContacts);
+    // if (retrievedContacts) setContacts(retrievedContacts);
+
+    const getAllContacts = async () => {
+      const allContacts = await retrieveContacts();
+      if (allContacts) setContacts (allContacts);
+    };
+
+    getAllContacts();
   }, []);
   
   useEffect(() => {
